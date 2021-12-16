987fe61ff75ec3121dc6e8cfa686475172c6b16e
Phillip Seryazi <seryaziphillip@gmail.com>
2021-12-16 18:40:45 +0300

    re-editing db url

--- storage/database.py
+++ storage/database.py
@@ -3,15 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-db_url = os.environ.get("DATABASE_URL").replace(
-    "postgres://", "postgresql://", 1)
-engine = create_engine(db_url)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
+engine = create_engine(os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://", 1))
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base=declarative_base()
 
 
 def get_db():
-    db = SessionLocal()
+    db=SessionLocal()
     try:
         yield db
     finally:
