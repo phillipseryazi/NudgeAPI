1d5f8622adea2cd3770041c8bfc703f8d32b4ba7
Phillip Seryazi <seryaziphillip@gmail.com>
2021-12-16 18:15:54 +0300

    editing db url to fix heroku error

--- storage/database.py
+++ storage/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine(os.environ["DATABASE_URL"].replace("postgres://","postgresql://",1))
+engine = create_engine(os.environ.get("DATABASE_URL").replace("postgres://","postgresql://",1))
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
