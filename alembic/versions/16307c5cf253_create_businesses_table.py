38d529b46113bca7caec79cbe8fee4a69a3663c6
Phillip Seryazi <seryaziphillip@gmail.com>
2021-12-17 15:48:05 +0300

    adding migrations for business table

---
+++ alembic/versions/16307c5cf253_create_businesses_table.py
@@ -0,0 +1,51 @@
+"""create businesses table
+
+Revision ID: 16307c5cf253
+Revises: 097dc0840a73
+Create Date: 2021-12-17 12:47:11.738198
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.sql.schema import PrimaryKeyConstraint, UniqueConstraint
+
+
+# revision identifiers, used by Alembic.
+revision = '16307c5cf253'
+down_revision = '097dc0840a73'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table("businesses",
+                    sa.Column("id", sa.Integer, nullable=False),
+                    sa.Column("name", sa.String, nullable=False),
+                    sa.Column("description", sa.Text, nullable=False),
+                    sa.Column("domain", sa.String, nullable=False),
+                    sa.Column("contacts", sa.JSON, nullable=False),
+                    sa.Column("email", sa.String, nullable=False),
+                    sa.Column("created_at",
+                              sa.TIMESTAMP(timezone=True),
+                              nullable=False,
+                              server_default=sa.text("now()")
+                              ),
+                    sa.Column("updated_at",
+                              sa.TIMESTAMP(timezone=True),
+                              nullable=False,
+                              server_default=sa.text("now()")
+                              ),
+                    sa.Column("user_id", sa.Integer, nullable=False),
+                    sa.ForeignKeyConstraint(
+                        ["user_id"],
+                        ["users.id"],
+                        ondelete="CASCADE",
+                        name="business_user_fk"),
+                    sa.PrimaryKeyConstraint("id"),
+                    sa.UniqueConstraint("email")
+                    )
+
+
+def downgrade():
+    op.drop_constraint("business_user_fk", table_name="businesses")
+    op.drop_table("businesses")
---
+++ app/businesses/models.py
@@ -0,0 +1,27 @@
+from typing import Text
+from sqlalchemy.sql.expression import text, true
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import JSON, TIMESTAMP, Integer, String
+from storage.database import Base
+
+
+class Business(Base):
+    __tablename__ = "businesses"
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    description = Column(Text, nullable=False)
+    domain = Column(String, nullable=False)
+    contacts = Column(JSON, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    created_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,
+                        server_default=text('now()')
+                        )
+    updated_at = Column(TIMESTAMP(timezone=True),
+                        nullable=False,
+                        server_default=text('now()')
+                        )
+    user_id = Column(Integer,
+                     ForeignKey("users.id", ondelete="CASCADE"),
+                     nullable=False
+                     )
